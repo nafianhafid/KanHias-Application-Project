485bbcd165f473a9e841af83743692cbb4bdec52
LutfiAlfianto <lutfirazer99@gmail.com>
2022-06-12 22:39:10 +0700

    API File with NodeJS

---
+++ Cloud Computing/config/database.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'barbers'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
